dc03bce49e561b00a59caa517c0cf591ebc378c0
Rishu <rishusingh1530@gmail.com>
2024-09-14 15:27:35 +0530

    files added

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar'
+import Coin from './pages/Coin/Coin'
+import Home from './pages/Home/Home'
+
+const App = () => {
+  return (
+    <div className='app'>
+     <Navbar />
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/coin/:coinId' element={<Coin/>}/>
+     </Routes>
+     <Footer/>
+    </div>
+  )
+}
+
+export default App
